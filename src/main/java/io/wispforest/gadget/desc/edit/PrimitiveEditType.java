95c1e51ddfe29b77cf9d3689578c01e1568df1e9
Basique <basiqueevangelist@yandex.ru>
2025-10-06 17:27:28 +0300

    bump version, remove owo sentienl, replace mixin with jankier fix

--- src/main/java/io/wispforest/gadget/desc/edit/PrimitiveEditType.java
+++ src/main/java/io/wispforest/gadget/desc/edit/PrimitiveEditType.java
@@ -1,7 +1,10 @@
 package io.wispforest.gadget.desc.edit;
 
 import io.wispforest.endec.Endec;
+import io.wispforest.endec.annotations.SealedPolymorphic;
 
+// This makes ReflectiveEndecBuilder not want to try to wrap this into an ObjectEndec, which would fail.
+@SealedPolymorphic
 public interface PrimitiveEditType<T> {
     Endec<PrimitiveEditType<?>> ENDEC = PrimitiveEditTypes.ENDEC;
 
